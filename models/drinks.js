4ecde7bc8bc2c13d16534066523b15d379f6fd19
Richard <ricky.cary1@gmail.com>
2022-11-12 12:04:08 -0600

    Route to drinks set up

---
+++ models/drinks.js
@@ -0,0 +1,42 @@
+const drinks = [
+    {
+      name: 'cruddy mary',
+      price: 132,
+      image: 'https://i.imgur.com/Va5iIw5.jpg'
+    },
+    {
+      name: 'index on the beach',
+      price: 68,
+      image: 'https://i.imgur.com/XV2aPa2.jpg'
+    },
+    {
+      name: 'hack & coke',
+      price: 1,
+      image: 'https://i.imgur.com/rLOXFRI.jpg'
+    },
+    {
+      name: 'whiskey-value pair',
+      price: 11,
+      image: 'https://i.imgur.com/a7rmkoS.jpg'
+    },
+    {
+      name: '404 horsemen',
+      price: 202,
+      image: 'https://i.imgur.com/FLucOLr.jpg'
+    },
+    {
+      name: 'if stateMint julep',
+      price: 2,
+      image: 'https://i.imgur.com/yaoK0Dg.jpg'
+    },
+    {
+      name: 'APIPA',
+      price: 43,
+      image: 'https://i.imgur.com/qAhA7pi.jpg'
+    },
+    {
+      name: 'node to joy',
+      price: 56,
+      image: 'https://i.imgur.com/MbVdwZz.jpg'
+    }
+  ]
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // Import express
+const { response } = require('express');
 const express = require('express');
 // Create the application object
 const app = express();
@@ -13,6 +14,24 @@ app.use((request, response, next) => {
 app.get("/", (request, response) => {
     response.send('<h1>Welcome to the Gitput App</h1>')
 })
+
+app.use("/static", express.static("static"))
+app.use("/models", express.static("models"))
+
+
+// Deliver a single static file
+app.get("/staticfile", (reqest, response) => {
+    response.sendFile("/Users/rickycary/SEI/deliverables/unit2/Git-Pub/static/index.html")
+})
+
+app.get("/drinks", (request, response) => {
+    response.sendFile("/Users/rickycary/SEI/deliverables/unit2/Git-Pub/models/drinks.js")
+})
+
+app.get("/template", (request, response) => {
+    response.render("first.ejs")
+})
+
 
 app.listen(3000, () => {
     console.log("Server is listening on port 3000")
