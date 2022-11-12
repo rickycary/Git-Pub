1f0de088e7436f6dea16fa28e8804e42cf847cb5
Richard <ricky.cary1@gmail.com>
2022-11-12 11:31:33 -0600

    Welcome to the Gitpub App

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 // Import express
 const express = require('express');
+// Create the application object
 const app = express();
 
+// Register middleware (In between step)
+app.use((request, response, next) => {
+    console.log(request.url)
+    next()
+})
+
+// Register a route (end step)
+app.get("/", (request, response) => {
+    response.send('<h1>Welcome to the Gitput App</h1>')
+})
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000")
+})
